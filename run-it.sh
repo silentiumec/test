d6cd76500d88a45aa107f1b038dfbd335b7233f1
silentiumec <silentiumec@gmail.com>
2019-03-04 20:12:10 +0200

    test

--- run-it.sh
+++ run-it.sh
@@ -5,4 +5,3 @@ npm run webdriver &
 # Wait for port 4444 to be listening connections
 chmod +x wait-for-it.sh && ./wait-for-it.sh -t 60 127.0.0.1:4444 -- echo "driver is up"
 npm run test
-wait-for-it.sh
